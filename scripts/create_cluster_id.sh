e69d0b670993900dfdde47e0316bb7e6616c2c62
yhao3 <kyrieeeee2@gmail.com>
2024-10-01 14:06:55 +0800

    Update file paths for cluster ID in docker-compose.yml and create_cluster_id.sh

--- scripts/create_cluster_id.sh
+++ scripts/create_cluster_id.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-file_path="/tmp/clusterID/clusterID"
+file_path="/var/tmp/clusterID/clusterID"
+dir_path="/var/tmp/clusterID"
+
+if [ ! -d "$dir_path" ]; then
+  mkdir -p "$dir_path"
+  echo "Directory $dir_path has been created."
+fi
 
 if [ ! -f "$file_path" ]; then
-  /bin/kafka-storage random-uuid > /tmp/clusterID/clusterID
+  /bin/kafka-storage random-uuid > "$file_path"
   echo "Cluster ID has been created..."
 fi
+
+chmod 755 "$dir_path"
+chmod 644 "$file_path"
